6aecae88e43bf005f79382131ae7280f87950e68
Дамир Сабиров <s-damir@bk.ru>
2022-02-20 20:27:37 +0300

    Добавьте файлы проекта.

---
+++ MyCompany/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿//Controller для управления Админом "Главной страницей"
+using Microsoft.AspNetCore.Mvc;
+using MyCompany.Domain;
+
+namespace MyCompany.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly DataManager dataManager;
+
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(dataManager.ServiceItems.GetServiceItems());
+        }
+    }
+}
---
+++ MyCompany/Domain/Entities/TextField.cs
@@ -0,0 +1,17 @@
+﻿//Entity (сущность) сайта с описанием полей для настроек страницы "Контакты"
+using System.ComponentModel.DataAnnotations;
+
+namespace MyCompany.Domain.Entities
+{
+    public class TextField : EntityBase
+    {
+        [Required]
+        public string CodeWord { get; set; }
+
+        [Display(Name = "Название страницы (заголовок)")]
+        public override string Title { get; set; } = "Информационная страница";
+
+        [Display(Name = "Cодержание страницы")]
+        public override string Text { get; set; } = "Содержание заполняется администратором";
+    }
+}
